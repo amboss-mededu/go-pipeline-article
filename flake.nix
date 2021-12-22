45ee3ef48283b7e171a079d7ae7810d76056303f
Florian Beeres <yuuki@protonmail.com>
2021-12-22 19:25:35 +0100

    Update flake
    Update flake inputs and use the now available go_1_17 instead of the
    overlay

--- flake.nix
+++ flake.nix
@@ -4,29 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    golang.url = "https://dl.google.com/go/go1.17.src.tar.gz";
-    golang.flake = false;
   };
 
-  outputs = { self, nixpkgs, golang, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (self: super: {
-            go = super.go.overrideAttrs
-              (old: rec {
-                version = "1.17";
-                src = golang;
-              });
-          })
-        ];
-      };
+      let pkgs = import nixpkgs { inherit system; };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.go
+            pkgs.go_1_17
             pkgs.gotools
             pkgs.golangci-lint
             pkgs.gopls
