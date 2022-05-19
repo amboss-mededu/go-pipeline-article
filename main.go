e542963e3ebe98645dc6ac23905be393fec2abc8
Florian Beeres <yuuki@protonmail.com>
2022-05-19 11:40:56 +0200

    Improve the example
    - Create and return the value and error channel in each stage
    - Limit parallelism to 2 for demonstration purposes
    - Merge the error channels

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-  "errors"
 	"context"
+	"errors"
 	"log"
 	"strings"
+	"sync"
 	"time"
 
 	"golang.org/x/sync/semaphore"
@@ -50,47 +51,96 @@ func sink(ctx context.Context, cancelFunc context.CancelFunc, values <-chan stri
 	}
 }
 
-func step[In any,Out any](
-  ctx context.Context,
-  inputChannel <-chan In,
-  outputChannel chan Out,
-  errorChannel chan error,
-  fn func(In) (Out, error),
-) {
-	defer close(outputChannel)
+func step[In any, Out any](
+	ctx context.Context,
+	inputChannel <-chan In,
+	fn func(In) (Out, error),
+) (chan Out, chan error) {
+	outputChannel := make(chan Out)
+	errorChannel := make(chan error)
 
-	limit := runtime.NumCPU()
+	limit := int64(2)
+	// Use all CPU cores to maximize efficiency. We'll set the limit to 2 so you
+	// can see the values being processed in batches of 2 at a time, in parallel
+	// limit := int64(runtime.NumCPU())
 	sem1 := semaphore.NewWeighted(limit)
 
-	for s := range inputChannel {
-		select {
-		case <-ctx.Done():
-			log.Print("1 abort")
-			break
-		default:
-		}
+	go func() {
+		defer close(outputChannel)
+		defer close(errorChannel)
 
-		if err := sem1.Acquire(ctx, 1); err != nil {
-			log.Printf("Failed to acquire semaphore: %v", err)
-			break
-		}
-
-		go func(s In) {
-			defer sem1.Release(1)
-			time.Sleep(time.Second * 3)
-
-			result, err := fn(s)
-			if err != nil {
-				errorChannel <- err
-			} else {
-				outputChannel <- result
+		for s := range inputChannel {
+			select {
+			case <-ctx.Done():
+				break
+			default:
 			}
-		}(s)
+
+			if err := sem1.Acquire(ctx, 1); err != nil {
+				log.Printf("Failed to acquire semaphore: %v", err)
+				break
+			}
+
+			go func(s In) {
+				defer sem1.Release(1)
+				time.Sleep(time.Second * 3)
+
+				result, err := fn(s)
+				if err != nil {
+					errorChannel <- err
+				} else {
+					outputChannel <- result
+				}
+			}(s)
+		}
+
+		if err := sem1.Acquire(ctx, limit); err != nil {
+			log.Printf("Failed to acquire semaphore: %v", err)
+		}
+	}()
+
+	return outputChannel, errorChannel
+}
+
+func Merge[T any](ctx context.Context, cs ...<-chan T) <-chan T {
+	var wg sync.WaitGroup
+	out := make(chan T)
+
+	output := func(c <-chan T) {
+		defer wg.Done()
+		for n := range c {
+			select {
+			case out <- n:
+			case <-ctx.Done():
+				return
+			}
+		}
 	}
 
-	if err := sem1.Acquire(ctx, limit); err != nil {
-		log.Printf("Failed to acquire semaphore: %v", err)
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
 	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func transformA(s string) (string, error) {
+	return strings.ToLower(s), nil
+}
+
+func transformB(s string) (string, error) {
+  // Comment this out to see the pipeline finish successfully
+	if s == "foo" {
+		return "", errors.New("oh no")
+	}
+
+	return strings.Title(s), nil
 }
 
 func main() {
@@ -104,30 +154,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	stage1 := make(chan string)
-	errorChannel := make(chan error)
+	step1results, step1errors := step(ctx, readStream, transformA)
+	step2results, step2errors := step(ctx, step1results, transformB)
+	allErrors := Merge(ctx, step1errors, step2errors)
 
-	transformA := func(s string) (string, error) {
-		return strings.ToLower(s), nil
-	}
-
-	go func() {
-		step(ctx, readStream, stage1, errorChannel, transformA)
-	}()
-
-	stage2 := make(chan string)
-
-	transformB := func(s string) (string, error) {
-		if s == "foo" {
-			return "", errors.New("oh no")
-		}
-
-		return strings.Title(s), nil
-	}
-
-	go func() {
-		step(ctx, stage1, stage2, errorChannel, transformB)
-	}()
-
-	sink(ctx, cancel, stage2, errorChannel)
+	sink(ctx, cancel, step2results, allErrors)
 }
